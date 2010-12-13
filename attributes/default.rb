3bfc39820cf3545939c90ad95bedc39dfd5e7853
Fletcher Nichol <fnichol@nichol.ca>
2010-12-12 21:04:46 -0700

    Start to add deployment user for rails recipes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 default[:webapp][:apps] = []
 
+default[:webapp][:users][:deploy][:deploy_keys] = []
+
 default[:webapp][:default][:listen_ports] = [ 80 ]
-default[:webapp][:default][:user] = "root"
+default[:webapp][:default][:user] = "deploy"
+
