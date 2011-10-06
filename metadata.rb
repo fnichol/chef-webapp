44bc9d6461c46f2b878fb7b20ee5ab8e451ac8dd
Fletcher Nichol <fnichol@nichol.ca>
2011-10-06 16:54:47 -0600

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,17 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures hosted web applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1"
+version          "0.1.1"
+
+supports "ubuntu"
+supports "debian"
+supports "suse"
+
+depends "apache2"
+depends "nginx"
+
+recommends "rvm"
+recommends "rvm_passenger"
+recommends "php"
+
+recipe "webapp"
